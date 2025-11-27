610428d20f8db32533a674507d9f89684b60edf8
Alex <alexukrok@gmail.com>
2025-11-27 22:55:34 +0200

    Started implementing Configuration Services

---
+++ AlexuKkPortfolioAPI/Services/AppConfigService.cs
@@ -0,0 +1,58 @@
+﻿using AlexuKkPortfolioAPI.DTOs.AppConfigDTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlexuKkPortfolioAPI.Services
+{
+    public class AppConfigService(DbContext context) : IAppConfigService
+    {
+        public Task<GetContactInfoDto> GetContactInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetCurrentProjDetails> GetCurrentProjDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetDonationInfoDTO> GetDonationInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetFlashDetailsDTO> GetFlashDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetGitHubDTO> GetGitHubDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatchContactInfoDTO> PatchContactInfo(PatchContactInfoDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatchCurrentProjDetailsDTO> PatchCurrentProjDetails(PatchCurrentProjDetailsDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatchDonationInfoDTO> PatchDonationinfo(PatchDonationInfoDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatchFlashDetailsDTO> PatchFlashDetails(PatchFlashDetailsDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatchGitHubDTO> PatchGitHubDetails(PatchGitHubDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
