1d71fd5930065d5e8e19d6305e5078a7c75c7458
Alex <alexukrok@gmail.com>
2025-11-28 21:57:16 +0200

    Implemented: PATCH Controllers for every Service

---
+++ AlexuKkPortfolioAPI/Controllers/PatchConfigController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AlexuKkPortfolioAPI.DTOs;
+using AlexuKkPortfolioAPI.Services;
+using AlexuKkPortfolioAPI.DTOs.AppConfigDTOs;
+
+namespace AlexuKkPortfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatchConfigController(IAppConfigService AppConfigService) : ControllerBase
+    {
+        [HttpPatch("/GitHub")]
+        public async Task<IActionResult> PatchGitHubInfo([FromBody] PatchGitHubDTO dto)
+        {
+            var result = await AppConfigService.PatchGitHubDetails(dto);
+            if (result == null)
+            {
+                return BadRequest("There are no fields provided");
+            }
+            return Ok(result);
+        }
+
+        [HttpPatch("/ContactInfo")]
+        public async Task<IActionResult> PatchContactInfo([FromBody] PatchContactInfoDTO dto)
+        {
+            var result = await AppConfigService.PatchContactInfo(dto);
+            if (result == null)
+            {
+                return BadRequest("There are no fields provided");
+            }
+            return Ok(result);
+        }
+
+        [HttpPatch("/DonationsInfo")]
+        public async Task<IActionResult> PatchDonationsInfo([FromBody] PatchDonationInfoDTO dto)
+        {
+            var result = await AppConfigService.PatchDonationinfo(dto);
+            if (result == null)
+            {
+                return BadRequest("There are no fields provided");
+            }
+            return Ok(result);
+        }
+
+        [HttpPatch("/FlashMessage")]
+        public async Task<IActionResult> PatchFlashInfo([FromBody] PatchFlashDetailsDTO dto)
+        {
+            var result = await AppConfigService.PatchFlashDetails(dto);
+            if (result == null)
+            {
+                return BadRequest("There are no fields provided");
+            }
+            return Ok(result);
+        }
+
+        [HttpPatch("/CurrentProject")]
+        public async Task<IActionResult> PatchCurrentProjInfo([FromBody] PatchCurrentProjDetailsDTO dto)
+        {
+            var result = await AppConfigService.PatchCurrentProjDetails(dto);
+            if (result == null)
+            {
+                return BadRequest("There are no fields provided");
+            }
+            return Ok(result);
+        }
+    }
+}
