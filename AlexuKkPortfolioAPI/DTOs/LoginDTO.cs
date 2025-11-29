08e53bdc20da3c4f5f6fcdb1838d50a91ec3b32d
Alex <alexukrok@gmail.com>
2025-11-29 20:55:04 +0200

    Implemented: Login DTO, Interface for auth service

---
+++ AlexuKkPortfolioAPI/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlexuKkPortfolioAPI.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
+
---
+++ AlexuKkPortfolioAPI/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using AlexuKkPortfolioAPI.Entities;
+using AlexuKkPortfolioAPI.DTOs;
+
+
+namespace AlexuKkPortfolioAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<string> Login(LoginDTO dto);
+
+        Task<User> Register(LoginDTO dto);
+    }
+}
