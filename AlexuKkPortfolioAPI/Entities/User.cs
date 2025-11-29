cf3739ce2f15cc0ce25b1f9837cab21f7b268500
Alex <alexukrok@gmail.com>
2025-11-29 20:27:06 +0200

    Implemented: User Entity

---
+++ AlexuKkPortfolioAPI/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlexuKkPortfolioAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Username { get; set; }
+
+        [Required]
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+
+        [Required]
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+        [Required]
+        [MaxLength(10)]
+        public string Role { get; set; } = "Admin";
+    }
+}
